b281e3aa3621fd2e55a447fa1a06e839e26a8d14
Domt301 <leonardod.trimarchi@gmail.com>
2017-04-10 18:30:26 -0400

    got the thing working for chapter 3 start

--- app/components/product-detail/product-detail.ts
+++ app/components/product-detail/product-detail.ts
@@ -1,20 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
-    selector: 'auction-product-page',
-    template: `
+  selector: 'auction-product-page',
+  template: `
     <div>
-    <img src="http://placehold.it/820x320">
-    <h4>{{productTitle}}</h4>
+      <img src="http://placehold.it/820x320">
+      <h4>{{productTitle}}</h4>
     </div>
-    `
+  `
 })
+export default class ProductDetailComponent {
+  productTitle: string;
 
-export default class ProductDetailComponent{
-    productTitle: string;
-
-    constructor(route: ActivatedRoute){
-        this.productTitle = route.snapshot.params['prodTitle'];
-    }
+  constructor(route: ActivatedRoute){
+    this.productTitle = route.snapshot.params['prodTitle'];
+  }
 }
